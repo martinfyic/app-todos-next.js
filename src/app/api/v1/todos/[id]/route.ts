702ae6c46e5e1c5598541b070778e74b1381b743
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-20 19:41:40 -0300

    [update]: agregue endpoint PUT para actualizar TODO

--- src/app/api/v1/todos/[id]/route.ts
+++ src/app/api/v1/todos/[id]/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse, NextRequest } from 'next/server';
 
+import { boolean, object, string } from 'yup';
+
 import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
 
 interface Segments {
   params: {
@@ -8,14 +11,20 @@ interface Segments {
   };
 }
 
-export async function GET(request: Request, { params }: Segments) {
-  const data = await prisma.todo.findUnique({
+//* Helper Get todo ----
+const getTodo = async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findUnique({
     where: {
-      id: params.id,
+      id: id,
     },
   });
+  return todo;
+};
 
-  if (!data) {
+export async function GET(request: Request, { params }: Segments) {
+  const todo = await getTodo(params.id);
+
+  if (!todo) {
     return NextResponse.json(
       {
         message: `Todo with ID: ${params.id} not found`,
@@ -25,6 +34,53 @@ export async function GET(request: Request, { params }: Segments) {
   }
 
   return NextResponse.json({
-    data,
+    date: `${new Date().toLocaleString()}`,
+    url: request.url,
+    method: request.method,
+    data: todo,
   });
+}
+
+//* Validación de los objetos PUT ----
+const putSchema = object({
+  description: string().optional(),
+  complete: boolean().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const todo = await getTodo(params.id);
+
+  if (!todo) {
+    return NextResponse.json(
+      {
+        message: `Todo with ID: ${params.id} not found`,
+      },
+      { status: 404 }
+    );
+  }
+
+  try {
+    const { complete, description } = await putSchema.validate(await request.json());
+    const updateTodo = await prisma.todo.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        complete,
+        description,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        date: `${new Date().toLocaleString()}`,
+        url: request.url,
+        method: request.method,
+        data: updateTodo,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
