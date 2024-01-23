74d47591d0ac9791f811a7c74809d20082eb960a
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-22 21:05:39 -0300

    [update]: agregue link para ir a dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-around p-24'>
       <h1 className='inline-block bg-gradient-to-tl from-purple-300 via-purple-400 to-purple-800 bg-clip-text text-center text-7xl font-semibold text-transparent'>
         TODO list
       </h1>
-      <div className='rounded bg-gradient-to-r from-purple-500 to-purple-900 px-4 py-2'>
-        <p className='text-xl text-white/85'>Martin Ferreira Yic</p>
-      </div>
+      <Link
+        href='/dashboard'
+        className='rounded bg-gradient-to-r from-purple-500 to-purple-900 px-4 py-2'
+      >
+        <p className='text-xl text-white/85'>Dashboard</p>
+      </Link>
     </main>
   );
 }
