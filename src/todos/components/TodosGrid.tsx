640dc9407d806b20ddccdf6195f207d29606eede
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-25 20:52:43 -0300

    [feat]: agregue logica para el toogleTodo
    Este cambio permite cambiar el estado del TODO desde la base de datos

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,17 +1,31 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { Todo } from '@prisma/client';
+
 import { TodoItem } from '.';
+import * as todosApi from '../helpers';
 
 interface Props {
   todos?: Todo[];
 }
 
 export const TodosGrid = ({ todos = [] }: Props) => {
+  const router = useRouter();
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    const updateTodo = await todosApi.updateTodo(id, complete);
+
+    router.refresh();
+
+    return updateTodo;
+  };
+
   return (
     <div className='grid grid-cols-1 gap-2 sm:grid-cols-3'>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
       ))}
     </div>
   );
