3ecb996a6bfbb907253f2e8aa4bedca5cf572350
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-25 20:51:16 -0300

    [update]: implemente el toodleTodo
    esto permite cambiar el estado de complete

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Todo } from '@prisma/client';
 
 import styles from './TodoItem.module.css';
@@ -5,13 +7,15 @@ import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 
 interface Props {
   todo: Todo;
+  toggleTodo: (id: string, complete: boolean) => Promise<Todo | void>;
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
   return (
     <div className={todo.complete ? styles.todoDone : styles.todoPending}>
       <div className='flex flex-col items-center justify-start gap-4 text-gray-700 sm:flex-row'>
         <div
+          onClick={() => toggleTodo(todo.id, !todo.complete)}
           className={`flex cursor-pointer rounded-md bg-blue-100 p-2 hover:bg-opacity-60 ${todo.complete ? 'bg-blue-100' : 'bg-red-100'}`}
         >
           {todo.complete ? (
