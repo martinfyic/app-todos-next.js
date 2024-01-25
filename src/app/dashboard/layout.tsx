454390091c7ef46a967c7e03fc840a0b0a7b4c74
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-25 20:47:28 -0300

    [chore]: elimine comentarios

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,11 +6,8 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <>
       <Sidebar />
 
-      {/* Main Layout content - Contenido principal del Layout */}
       <div className='mb-6 ml-auto min-h-screen lg:w-[75%] xl:w-[80%] 2xl:w-[85%]'>
         <TopMenu />
-
-        {/* TODO: Contenido en el Layout.tsx */}
         <div className='px-6 pt-6'>{children}</div>
       </div>
     </>
