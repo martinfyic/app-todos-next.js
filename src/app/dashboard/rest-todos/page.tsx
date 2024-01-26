f619bec6cf6a3edfb8dac472506933459003baa9
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-26 20:23:10 -0300

    [update]: agrego h1 en src/app/dashboard/rest-todos/page.tsx

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -11,6 +11,9 @@ export default async function RestTodosPage() {
 
   return (
     <>
+      <h1 className='mb-24 bg-gradient-to-r from-sky-600 via-cyan-400 to-purple-200 bg-clip-text text-center text-7xl font-semibold text-transparent'>
+        Rest TODOS
+      </h1>
       <div className='mx-5 mb-16 h-auto w-full px-5'>
         <NewTodo />
       </div>
