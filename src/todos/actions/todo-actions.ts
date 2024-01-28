f28cbf28f570d001746f1eef5d1e382fbbdad274
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-28 16:30:38 -0300

    [refactor]: add create todo and delete all completed todos

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+// TODO => Como mejora podríamos enviar un parámetro adicional a los server action que le indique el path en que estamos para utilizarlo en el revalidate.
+
 import { revalidatePath } from 'next/cache';
 
 import prisma from '@/lib/prisma';
@@ -20,4 +22,27 @@ export const toggleTodo = async (id: string, complete: boolean): Promise<Todo> =
   revalidatePath('/dashboard/server-todos');
 
   return updateTodo;
+};
+
+export const addTodo = async (
+  description: string
+): Promise<Todo | { message: string }> => {
+  try {
+    const todo = await prisma.todo.create({ data: { description } });
+    revalidatePath('/dashboard/server-todos');
+    return todo;
+  } catch (error) {
+    return {
+      message: 'Error creando TODO',
+    };
+  }
+};
+
+export const deleteCompleted = async (): Promise<void> => {
+  await prisma.todo.deleteMany({
+    where: {
+      complete: true,
+    },
+  });
+  revalidatePath('/dashboard/server-todos');
 };
