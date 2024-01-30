3af573382a84e689e46a1e03cd89f836f7e229b2
Martin Ferreira <martin.f.yic@gmail.com>
2024-01-29 21:51:52 -0300

    [refactor]: ahora la entrada de la app es /dashboard
    al ingresar a la app se redirecciona a /dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-around p-24'>
-      <h1 className='inline-block bg-gradient-to-tl from-purple-300 via-purple-400 to-purple-800 bg-clip-text text-center text-7xl font-semibold text-transparent'>
-        TODO list
-      </h1>
-      <Link
-        href='/dashboard'
-        className='rounded bg-gradient-to-r from-purple-500 to-purple-900 px-4 py-2'
-      >
-        <p className='text-xl text-white/85'>Dashboard</p>
-      </Link>
-    </main>
-  );
+  redirect('/dashboard');
 }
