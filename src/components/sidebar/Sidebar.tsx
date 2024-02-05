a9bb404dfc82837cf5a96850e3ae9d7c32cb55ce
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-04 22:04:34 -0300

    [UI] muestro rol del usuario utilizando la session

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -54,6 +54,7 @@ export const Sidebar = async () => {
   const userAvatarUrl =
     session?.user?.image ??
     'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+  const userRoles = session?.user?.roles ?? ['client'];
 
   return (
     <aside className='fixed top-0 z-10 ml-[-100%] flex h-screen w-full flex-col justify-between border-r bg-white px-6 pb-3 transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]'>
@@ -80,7 +81,9 @@ export const Sidebar = async () => {
           <h5 className='mt-4 hidden text-xl font-semibold text-gray-600 lg:block'>
             {userName}
           </h5>
-          <span className='hidden text-gray-400 lg:block'>Admin</span>
+          <span className='hidden capitalize text-gray-400 lg:block'>
+            {userRoles.join(', ')}
+          </span>
         </div>
 
         <ul className='mt-8 space-y-2 tracking-wide'>
