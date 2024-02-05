175ffb52910023cc5146c8acb7d93846034add55
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-04 22:02:24 -0300

    [refactor] configure adapter, session y callbacks en authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { Adapter } from 'next-auth/adapters';
+import prisma from '@/lib/prisma';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? '',
@@ -13,6 +18,38 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET ?? '',
     }),
   ],
+
+  session: {
+    strategy: 'jwt',
+  },
+
+  callbacks: {
+    async signIn() {
+      return true;
+    },
+
+    async jwt({ token }) {
+      const dbUser = await prisma.user.findUnique({
+        where: { email: token.email ?? '' },
+      });
+      if (dbUser?.isActive === false) {
+        throw new Error('Usuario no activo');
+      }
+      token.roles = dbUser?.roles ?? ['no-roles'];
+      token.id = dbUser?.id ?? 'no-uuid';
+      token.isActive = dbUser?.isActive;
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session && session.user) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+        session.user.isActive = token.isActive;
+      }
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
