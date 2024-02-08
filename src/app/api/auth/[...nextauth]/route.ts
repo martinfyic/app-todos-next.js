4a9117db3b8b8a7ef1dafb277d4b22fd9e058d77
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-07 22:01:25 -0300

    [feat] agregur CredentialsProvider en authOptions para custom login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,43 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
+
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { Adapter } from 'next-auth/adapters';
+
 import prisma from '@/lib/prisma';
+import { signInEmailPassword } from '@/auth/actions/auth-actions';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
 
   providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Correo Electrónico',
+          type: 'email',
+          placeholder: 'user@example.com',
+        },
+        password: { label: 'Contraseña', type: 'password', placeholder: '******' },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(credentials!.email, credentials!.password);
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        }
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null;
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? '',
       clientSecret: process.env.GOOGLE_SECRET ?? '',
